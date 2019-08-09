6afe4235c4d07530b0501b8eabc7e78990298c0a
rpk79 <rajkharvar1808@gmail.com>
2019-08-10 00:40:53 +0530

    another commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-    string public candidate;
-    string public anotherCandidate;
+    // string public candidate;
+    // string public anotherCandidate;
+    // mapping allows to return a value based on key
+    // In this case it is eth address and return type is Candidate
+    // here candidates is a function that iterate over it
+    struct Candidate {
+        uint id;
+        uint votersCount;
+        string name;
+    }
 
+    uint public candidateCount;
     constructor() public {
-        candidate = "Candidate 1";
-        anotherCandidate = "Candidate 2";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    mapping(uint => Candidate) public candidates;
+
+    function addCandidate (string memory _name) private {
+        candidateCount ++;
+        candidates[candidateCount] = Candidate(candidateCount, 0, _name);
     }
 }
