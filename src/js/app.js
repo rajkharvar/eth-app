6ee00e9465038a454404d1d30ba124d3155a8f6b
rpk79 <rajkharvar1808@gmail.com>
2019-08-15 17:25:30 +0530

    not able to get list of candidates but able to connect metamask with contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,15 @@
+$.ajaxSetup({ async: false })
 App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
 
+  // init function
   init: function () {
     return App.initWeb3()
   },
 
+  // init connection of client side application with local-blockchain
   initWeb3: async function () {
     if (typeof web3 !== 'undefined') {
       // if a instance if provided by metamask
@@ -20,36 +23,57 @@ App = {
     return App.initContract();
   },
 
+  // initContract from ABI
   initContract: function () {
     $.getJSON('Election.json', function (election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election)
       // connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider)
+      return App.render();
     })
-    return App.bindEvents();
   },
 
-  bindEvents: function () {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  render: function () {
+    var electionInstance
+    var loader = $('#loader')
+    var content = $('#content')
 
-  markAdopted: function (adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    loader.show()
+    content.show()
 
-  handleAdopt: function (event) {
-    event.preventDefault();
+    web3.eth.getCoinbase(function (err, account) {
+      if (err === null) {
+        App.account = account
+        $('#accountAddress').html("Your account Address:" + account)
+      }
+    })
 
-    var petId = parseInt($(event.target).data('id'));
+    App.contracts.Election.deployed().then(function (instance) {
+      electionInstance = instance
+      return electionInstance.candidateCount()
+    }).then(function (candidateCount) {
+      var candidateResults = $('#candidateResults')
+      candidateResults.empty()
 
-    /*
-     * Replace me...
-     */
+      for (var i = 1; i <= candidateResults; i++) {
+        electionInstance.candidates(i).then(function (candidate) {
+          console.log(candidate)
+          var id = candidate[0]
+          var name = candidate[1]
+          var voterCount = candidate[2]
+
+          var template = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voterCount + "</td></tr>"
+          candidateResults.append(template)
+          // var candidateOption ="<option value='" + id + "' >" + name + "</ option>"
+        })
+      }
+      loader.hide()
+      content.show()
+    }).catch(function (err) {
+      console.warn(err)
+    })
   }
-
 };
 
 $(function () {
